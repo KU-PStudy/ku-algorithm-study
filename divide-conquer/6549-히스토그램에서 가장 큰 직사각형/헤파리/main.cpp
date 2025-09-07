ea359106a78b9ab6bbe8abb71337de834dfe9f15
hepari <107000683+rosmontisu@users.noreply.github.com>
2025-09-07 20:51:52 +0900

    헤파리: 변수 수정

--- divide-conquer/6549-히스토그램에서 가장 큰 직사각형/헤파리/main.cpp
+++ divide-conquer/6549-히스토그램에서 가장 큰 직사각형/헤파리/main.cpp
@@ -1,5 +1,9 @@
+// 스터디 2주차 재귀&분할정복 복슴겸 + 3주차 투포인터 설명용 문제
 // Z처럼 탑다운 방식으로 접근 가능
+// 가장 큰 직사각형의 포함여부로 왼쪽절반/오른쪽절반/중간경계선걸침 3가지로 분할 후
 // 좌/우 재귀로 접근 + 중앙 투포인터로 탐색
+
+// 변수 부분 더 깔끔하게 수정
 
 #include <iostream>
 #include <vector>
@@ -10,25 +14,24 @@ vector<long long> h;
 
 long long func(int st, int ed)
 {
-    // 1. base case
     if (st == ed) return h[st];
 
-    // 2. 분할
+    // 1. 좌우 분할 후 재귀
     int mid = (st + ed) / 2;
+    long long ans = max(func(st, mid), func((mid + 1), ed));
 
-    // 3. 정복 - { st ~ mid }, { mid+1 ~ ed } 왼쪽 절반, 오른쪽 절반 재귀로 탐색
-    long long ans = max(func(st, mid), func(mid + 1, ed));
 
-    // 4. 결합 - { min, mid+1 } 겹치는 사각형
+    // 2. 투포인터로 모든 경우 확인
     int l = mid;
     int r = mid + 1;
-    long long height = min(h[l], h[r]); // 작은게 기준!
-
-    ans = max(ans, height * 2); 
+    
+    long long height = min(h[l], h[r]);
+    long long width = 2;
+    ans = max(ans, height * width);
 
     while (st < l || r < ed)
     {
-        if (r < ed && (st == l || h[l - 1] < h[r + 1]))
+        if (r < ed && (st == l || h[l - 1] < h[r + 1])) // l을 못움직이거나, r이 더 유리하거나
         {
             r++;
             height = min(height, h[r]);
@@ -38,8 +41,9 @@ long long func(int st, int ed)
             l--;
             height = min(height, h[l]);
         }
-        
-        ans = max(ans, height * (r - l + 1)); // h * 너비
+
+        width++;
+        ans = max(ans, height * width);
     }
 
     return ans;
